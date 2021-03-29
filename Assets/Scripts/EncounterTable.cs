4d775236f7d7f1acb6230700bed7a3dfbe5d5db8
zachary briggs <30901192+ZacharyBriggs@users.noreply.github.com>
2021-03-28 22:44:34 -0500

    moved scripts

--- Assets/Scripts/EncounterTable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewEncounterTable", menuName = "ScriptableObjects/Create New Encounter Table")]
-public class EncounterTable : ScriptableObject
-{
-    public List<Demon> encounterTable = new List<Demon>();
-}

652e68de72f3e831af29b532b7f3e14c5e67565e
zachary briggs <30901192+ZacharyBriggs@users.noreply.github.com>
2021-03-28 22:44:11 -0500

    code changes for moving in overworld

--- Assets/Scripts/OverworldManager.cs
+++ Assets/Scripts/OverworldManager.cs
@@ -6,6 +6,11 @@ public class OverworldManager : MonoBehaviour
 {
     public static OverworldManager instance;
     public EncounterTable encounters;
+    public GameObject playerGC;
+    public PlayerBattleScriptable playerScriptable;
+    public List<Demon> playerDemons = new List<Demon>();
+    public Room currentRoom;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,55 @@ public class OverworldManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            if (playerGC.transform.eulerAngles.y == 0)
+            {
+                if (currentRoom.connectedRoomN != null)
+                {
+                    currentRoom = currentRoom.connectedRoomN;
+                    playerGC.transform.position = currentRoom.playerMovePoint.position;
+                    playerGC.transform.position += new Vector3(0, 1, 0);
+                }
+            }
+            else if (playerGC.transform.eulerAngles.y == 180)
+            {
+                if (currentRoom.connectedRoomS != null)
+                {
+                    currentRoom = currentRoom.connectedRoomS;
+                    playerGC.transform.position = currentRoom.playerMovePoint.position;
+                    playerGC.transform.position += new Vector3(0, 1, 0);
+                }
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            playerGC.transform.Rotate(Vector3.up,90);
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            if (playerGC.transform.eulerAngles.y == 0)
+            {
+                if (currentRoom.connectedRoomS != null)
+                {
+                    currentRoom = currentRoom.connectedRoomS;
+                    playerGC.transform.position = currentRoom.playerMovePoint.position;
+                    playerGC.transform.position += new Vector3(0, 1, 0);
+                }
+            }
+            else if (playerGC.transform.eulerAngles.y == 180)
+            {
+                if (currentRoom.connectedRoomN != null)
+                {
+                    currentRoom = currentRoom.connectedRoomN;
+                    playerGC.transform.position = currentRoom.playerMovePoint.position;
+                    playerGC.transform.position += new Vector3(0, 1, 0);
+                }
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            playerGC.transform.Rotate(Vector3.up, -90);
+        }
     }
 }

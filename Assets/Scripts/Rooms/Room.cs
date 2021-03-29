45e20118e306045b697f9b4fcff3a4bcbbebf895
zachary briggs <30901192+ZacharyBriggs@users.noreply.github.com>
2021-03-28 22:45:28 -0500

    created room script for managing rooms in overworld + player movement

---
+++ Assets/Scripts/Rooms/Room.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public Transform playerMovePoint;
+    public Room connectedRoomN;
+    public Room connectedRoomE;
+    public Room connectedRoomS;
+    public Room connectedRoomW;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
